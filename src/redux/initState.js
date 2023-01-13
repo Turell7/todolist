8a3577b694876b01527bde75e3d6f39192a7bca0
Н С <nikolai3578@gmail.com>
2023-01-13 03:01:48 +0300

    Added data saving to localStorage

--- src/redux/initState.js
+++ src/redux/initState.js
@@ -1,4 +1,12 @@
+export const REDUX_LS_KEY = 'REDUX_LS_KEY'
+
 export const initialState = {
     todos: [],
     counter: 0
+}
+
+export const getInitialState = () => {
+    const stateLS = localStorage.getItem(REDUX_LS_KEY)
+
+    return stateLS ? JSON.parse(stateLS) : initialState
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore } from "redux"
-import { initialState } from "./initState"
+import { getInitialState, REDUX_LS_KEY } from "./initState"
 import { rootReducer } from "./reducers/rootReducer"
 
 
 
-export const store = createStore(rootReducer, initialState)
+export const store = createStore(rootReducer, getInitialState())
+
+store.subscribe(() => {
+    localStorage.setItem(REDUX_LS_KEY, JSON.stringify(store.getState()))
+})
